8fa4e9dd7a6c4e31933ba1ca3fdf6231bc7f8278
Mohammad Safir Adrián <safirmohammad33@gmail.com>
2026-04-21 15:08:35 +0200

    Remove Legacy Machine Learning/ folder

--- Machine Learning/softmax/submission-0.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from numpy.typing import NDArray
-
-
-class Solution:
-
-    def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
-        return np.round(np.exp(z-np.max(z))/(sum(np.exp(z-np.max(z)))), 4)
-        pass
